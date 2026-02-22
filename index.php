2d3e3f2650a56ce99b246b115421f75d50e5db44
Lesik Edelweiss <lesikedelweiss@disroot.org>
2026-02-22 16:35:03 +0200

    Initial commit for lab02

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Привет, мир!<br>";
+
+echo "Hello, World with echo!<br>";
+print "Hello, World with print!<br>";
+
+$days = 228;
+$message = "Все возвращаются на работу!";
+
+echo "Я точно никогда не сидел по статье " . $days;
+echo "<br>Нет ничего хорошего в том, что $message";
